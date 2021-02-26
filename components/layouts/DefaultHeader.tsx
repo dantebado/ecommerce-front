acc164004c9646d9695c95d332a4156c5d41c02a
Dante Bado <me@dantebado.com>
2021-02-26 12:04:30 -0300

    i18n redo

--- components/layouts/DefaultHeader.tsx
+++ components/layouts/DefaultHeader.tsx
@@ -1,11 +1,13 @@
+import useTranslation from 'next-translate/useTranslation'
+import Link from 'next/link'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { actionSignoutMagicLink } from '../../redux/reducers/LoggedUser'
 import { StateTypes } from '../../redux/Store'
-import Link from 'next/link'
 
 export default function DefaultHeader() {
   const loggedUser = useSelector((state: StateTypes) => state.loggedUser)
+  const { t, lang } = useTranslation('common')
   const dispatch = useDispatch()
 
   const logoutHandler = () => {
@@ -15,7 +17,7 @@ export default function DefaultHeader() {
   return (
     <header className="border-bottom py-3 text-center">
       <Link href="/">
-        <a><h2 className="mb-3">HEADER</h2></a>
+        <a><h2 className="mb-3">HEADER | {t('i-test')} | {lang}</h2></a>
       </Link>
       {
         loggedUser.magicToken ? <button className="px-4 py-2" onClick={logoutHandler}>Cerrar Sesión</button> : <Link href="/login"><a className="display-block">Iniciar Sesión</a></Link>
